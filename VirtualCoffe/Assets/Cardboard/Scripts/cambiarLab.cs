6d59decf1b50ad4619e335ab74dd44e1e888f64d
Juan David Sanchez <juan.david748@gmail.com>
2018-04-30 04:44:43 -0500

    Historia lab calidades

---
+++ VirtualCoffe/Assets/Cardboard/Scripts/cambiarLab.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class cambiarLab : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if(other.tag == "Player"){
+			SceneManager.LoadScene("labCalidades");
+		}
+	}	
+}
